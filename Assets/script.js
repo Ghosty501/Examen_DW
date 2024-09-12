c60ca7c9458a0fa46de9799e7a9334cb16c360b9
Ghosty501 <0253260@up.edu.mx>
2024-09-12 00:19:02 -0600

    Hasta el momento todo bien

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,28 @@
-function rotate(direction) {
-    var div = document.getElementById("selectedCircle");
-    if (direction === "right") {
-        div.style.transform = "rotate(90deg)";
-    } else if (direction === "left") {
-        div.style.transform = "rotate(-90deg)";
+// Object to store the rotation angle for each circle
+const circleRotations = {
+    circle1: 0,
+    circle2: 0,
+    circle3: 0,
+    circle4: 0,
+    circle5: 0
+  };
+  
+  let selectedCircleId = null;
+  
+  function selectCircle(circleId) {
+    selectedCircleId = circleId;
+    document.getElementById('selectedCircleText').textContent = `Selected ${circleId}`;
+  }
+  
+  function rotate(direction) {
+    if (selectedCircleId) {
+      // Update the rotation angle for the selected circle
+      circleRotations[selectedCircleId] += (direction === 'right' ? 15 : -15);
+  
+      // Get the selected circle element
+      const circleElement = document.getElementById(selectedCircleId);
+  
+      // Apply the rotation transform
+      circleElement.style.transform = `translate(-50%, -50%) rotate(${circleRotations[selectedCircleId]}deg)`;
     }
-}
+  }
